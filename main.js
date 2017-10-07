ef82d368778322529b55164c9164e79a04a26875
Valentin Commenge <txreplay@gmail.com>
2017-10-07 23:26:35 +0200

    Tray visible on all Workspaces on macOS

--- main.js
+++ main.js
@@ -81,6 +81,7 @@ const createWindow = () => {
       backgroundThrottling: false
     }
   })
+  window.setVisibleOnAllWorkspaces(true);
   window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
   // Hide the window when it loses focus
